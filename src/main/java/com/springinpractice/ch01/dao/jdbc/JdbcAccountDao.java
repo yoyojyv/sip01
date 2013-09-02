732f821fd2fc45ba2f9170f14b7d20682a86e9f2
yoyojyv <yoyojyv@gmail.com>
2013-09-02 13:01:26 +0900

    02. dataSource -> setter method를 이용하여 설정 하도록 변경

--- src/main/java/com/springinpractice/ch01/dao/jdbc/JdbcAccountDao.java
+++ src/main/java/com/springinpractice/ch01/dao/jdbc/JdbcAccountDao.java
@@ -3,19 +3,32 @@ package com.springinpractice.ch01.dao.jdbc;
 import com.springinpractice.ch01.dao.AccountDao;
 import org.apache.commons.dbcp.BasicDataSource;
 
+import javax.sql.DataSource;
+
 /**
- * JdbcAccountDao 가 org.apache.commons.dbcp.BasicDataSource 와 긴밀하게 연결되어있음
+ * 01. JdbcAccountDao 가 org.apache.commons.dbcp.BasicDataSource 와 긴밀하게 연결되어있음
+ *
+ * 02. dataSource -> setter method를 이용하여 설정 하도록 변경
+ *
  */
 public class JdbcAccountDao implements AccountDao {
 
-  private BasicDataSource dataSource;
+//  private BasicDataSource dataSource;
+//
+//  public JdbcAccountDao() {
+//    dataSource = new BasicDataSource();
+//    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//    dataSource.setUrl("jdbc:mysql://localhost:3306/springbook" + "?autoReconnect=true");
+//    dataSource.setUsername("root");
+//    dataSource.setPassword("");
+//  }
 
-  public JdbcAccountDao() {
-    dataSource = new BasicDataSource();
-    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-    dataSource.setUrl("jdbc:mysql://localhost:3306/springbook" + "?autoReconnect=true");
-    dataSource.setUsername("root");
-    dataSource.setPassword("");
+  private DataSource dataSource;
+
+  public  JdbcAccountDao() {}
+
+  public void setDataSource(DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
 }
---
+++ src/main/java/com/springinpractice/ch01/service/AccountService.java
@@ -0,0 +1,31 @@
+package com.springinpractice.ch01.service;
+
+import com.springinpractice.ch01.dao.jdbc.JdbcAccountDao;
+import org.apache.commons.dbcp.BasicDataSource;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AccountService {
+
+  private JdbcAccountDao accountDao;
+
+  public AccountService() {
+    try {
+      Properties props = new Properties();
+      InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("dataSource.properties");
+      props.load(inputStream);
+      BasicDataSource dataSource = new BasicDataSource();
+      dataSource.setDriverClassName(props.getProperty("driverClassName"));
+      dataSource.setUrl(props.getProperty("url"));
+      dataSource.setUsername(props.getProperty("username"));
+      dataSource.setPassword(props.getProperty("password"));
+      accountDao = new JdbcAccountDao();
+      accountDao.setDataSource(dataSource);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+}
