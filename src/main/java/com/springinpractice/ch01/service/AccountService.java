ea078e281c8b5e0729523116e601764fb07b9b3f
yoyojyv <yoyojyv@gmail.com>
2013-09-02 13:21:44 +0900

    03 spring 을 이용한 di 설정

--- src/main/java/com/springinpractice/ch01/service/AccountService.java
+++ src/main/java/com/springinpractice/ch01/service/AccountService.java
@@ -1,31 +1,44 @@
 package com.springinpractice.ch01.service;
 
+import com.springinpractice.ch01.dao.AccountDao;
 import com.springinpractice.ch01.dao.jdbc.JdbcAccountDao;
 import org.apache.commons.dbcp.BasicDataSource;
 
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * 02. 생성자에서 dataSource 를 직접 생성하고 있음. BasicDataSource 와 긴밀한 연결.
+ * 03. AccountDao -> setter 메소드를 이용한 DI
+ */
 public class AccountService {
 
-  private JdbcAccountDao accountDao;
+//  private JdbcAccountDao accountDao;
+//
+//  public AccountService() {
+//    try {
+//      Properties props = new Properties();
+//      InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("dataSource.properties");
+//      props.load(inputStream);
+//      BasicDataSource dataSource = new BasicDataSource();
+//      dataSource.setDriverClassName(props.getProperty("driverClassName"));
+//      dataSource.setUrl(props.getProperty("url"));
+//      dataSource.setUsername(props.getProperty("username"));
+//      dataSource.setPassword(props.getProperty("password"));
+//      accountDao = new JdbcAccountDao();
+//      accountDao.setDataSource(dataSource);
+//    } catch (Exception e) {
+//      throw new RuntimeException(e);
+//    }
+//
+//  }
 
-  public AccountService() {
-    try {
-      Properties props = new Properties();
-      InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("dataSource.properties");
-      props.load(inputStream);
-      BasicDataSource dataSource = new BasicDataSource();
-      dataSource.setDriverClassName(props.getProperty("driverClassName"));
-      dataSource.setUrl(props.getProperty("url"));
-      dataSource.setUsername(props.getProperty("username"));
-      dataSource.setPassword(props.getProperty("password"));
-      accountDao = new JdbcAccountDao();
-      accountDao.setDataSource(dataSource);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
+  private AccountDao accountDao;
 
+  public AccountService() {}
+
+  public void setAccountDao(AccountDao accountDao) {
+    this.accountDao = accountDao;
   }
 
 }
