6e7063f560fec29298a1e50147a2be624147d126
yoyojyv <yoyojyv@gmail.com>
2013-09-02 13:36:32 +0900

    04. 작업중

--- src/main/java/com/springinpractice/ch01/dao/AccountDao.java
+++ src/main/java/com/springinpractice/ch01/dao/AccountDao.java
@@ -1,6 +1,14 @@
 package com.springinpractice.ch01.dao;
 
+import com.springinpractice.ch01.model.Account;
+
+import java.util.List;
+
+/**
+ * 04. 인터페이스에 구현할 메소드 추가
+ */
 public interface AccountDao {
 
+  List<Account> findAll() throws Exception;
 
 }
