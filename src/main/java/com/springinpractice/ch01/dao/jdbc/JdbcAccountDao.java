c44619e042667d8f57b8d5a26ee5f51a34615472
yoyojyv <yoyojyv@gmail.com>
2013-09-03 13:03:29 +0900

    Merge remote-tracking branch 'remotes/origin/08'
    Conflicts:
    	src/main/java/com/springinpractice/ch01/dao/jdbc/JdbcAccountDao.java

--- src/main/java/com/springinpractice/ch01/dao/jdbc/JdbcAccountDao.java
+++ src/main/java/com/springinpractice/ch01/dao/jdbc/JdbcAccountDao.java
@@ -1,21 +1,44 @@
 package com.springinpractice.ch01.dao.jdbc;
 
 import com.springinpractice.ch01.dao.AccountDao;
+import com.springinpractice.ch01.model.Account;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 /**
- * JdbcAccountDao 가 org.apache.commons.dbcp.BasicDataSource 와 긴밀하게 연결되어있음
+ * 01. JdbcAccountDao 가 org.apache.commons.dbcp.BasicDataSource 와 긴밀하게 연결되어있음
+ *
+ * 02. dataSource -> setter method를 이용하여 설정 하도록 변경
+ *
  */
+// @Component
+@Repository
 public class JdbcAccountDao implements AccountDao {
 
-  private BasicDataSource dataSource;
+//  private BasicDataSource dataSource;
+//
+//  public JdbcAccountDao() {
+//    dataSource = new BasicDataSource();
+//    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//    dataSource.setUrl("jdbc:mysql://localhost:3306/springbook" + "?autoReconnect=true");
+//    dataSource.setUsername("root");
+//    dataSource.setPassword("");
+//  }
 
-  public JdbcAccountDao() {
-    dataSource = new BasicDataSource();
-    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-    dataSource.setUrl("jdbc:mysql://localhost:3306/springbook" + "?autoReconnect=true");
-    dataSource.setUsername("root");
-    dataSource.setPassword("");
+  private DataSource dataSource;
+
+  public  JdbcAccountDao() {}
+
+  public void setDataSource(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  public List<Account> findAll() throws Exception {
+    throw new UnsupportedOperationException("This method has not been implemented");
   }
 
 }
